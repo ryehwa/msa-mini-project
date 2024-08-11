f73db9534c33a6c53a36384cbc067fb19ee733b3
ryehwa <rhk415@gmail.com>
2024-08-11 20:23:40 +0900

    first init

---
+++ src/main/java/com/sparta/msaminiproject/MsaMiniProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.msaminiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaMiniProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaMiniProjectApplication.class, args);
+    }
+
+}
